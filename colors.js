ca6203c3c010c078475518b113a6ccd8fc99688a
owlSuri <98316357+owlsuri@users.noreply.github.com>
2022-02-02 19:45:12 +0900

    수정
    jQuery 사용

--- colors.js
+++ colors.js
@@ -1,20 +1,23 @@
 var Links = {
     setColor:function(color){
-      var alist = document.querySelectorAll('a')
-          var i=0;
-          while(i < alist.length){
-          alist[i].style.color= color;
-          i=i+1;
-    }
-  }
+//       var alist = document.querySelectorAll('a')
+//           var i=0;
+//           while(i < alist.length){
+//           alist[i].style.color= color;
+//           i=i+1;
+//     }주석처리한 부분과 똑같은 것을 jQuery로 쓰면 아래와 같다.
+$('a').css('color', color);
+  } 
 }
   var Body = {
       setColor:function(color){
-        document.querySelector('body').style.color=color;
-      },
+       // document.querySelector('body').style.color=color;
+        $('body').css('color', color);
+    },
       setBackgroundColor:function(color){
-        document.querySelector('body').style.backgroundColor=color;
-      }
+       // document.querySelector('body').style.backgroundColor=color;
+       $('body').css('backgroundColor', color);
+    }
     }
 
   function nightDayHandler(self){
